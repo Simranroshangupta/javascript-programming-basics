ed8dfa7cd35741b79d06b84413ab735020290ad9
Simranroshangupta <guptasimransushma@gmail.com>
2026-07-05 18:26:40 +0530

    Add for loop example

---
+++ loops/ForLoop.js
@@ -0,0 +1,37 @@
+// ForLoop.js
+// Demonstrates the for loop in JavaScript.
+
+for (let number = 1; number <= 5; number++) {
+
+    console.log("Number :", number);
+}
+
+/*
+==========================================
+Explanation
+
+A for loop is used when the
+number of iterations is known.
+
+It has three parts:
+
+1. Initialization
+2. Condition
+3. Update
+
+Syntax
+
+for(initialization; condition; update) {
+    // Code
+}
+
+Expected Output
+
+Number : 1
+Number : 2
+Number : 3
+Number : 4
+Number : 5
+
+==========================================
+*/
