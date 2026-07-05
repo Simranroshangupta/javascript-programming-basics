0fe811349288a7a550bc00fd059b89aadf90e9a8
Simranroshangupta <guptasimransushma@gmail.com>
2026-07-05 18:47:46 +0530

    Add function expression example

---
+++ functions/FunctionExpression.js
@@ -0,0 +1,33 @@
+// FunctionExpression.js
+// Demonstrates function expressions in JavaScript.
+
+const greetUser = function (name) {
+
+    console.log("Hello,", name + "!");
+};
+
+greetUser("Simran");
+
+/*
+==========================================
+Explanation
+
+A function expression stores
+a function inside a variable.
+
+The function can then be
+called using the variable name.
+
+When to Use
+
+Use function expressions when
+you want to assign a function
+to a variable or pass it
+as an argument.
+
+Expected Output
+
+Hello, Simran!
+
+==========================================
+*/
