eb63c3a3dbc75694d8baa8588d60bcef68cfd37e
Simranroshangupta <guptasimransushma@gmail.com>
2026-07-05 18:46:17 +0530

    Add arrow function example

---
+++ functions/ArrowFunction.js
@@ -0,0 +1,31 @@
+// ArrowFunction.js
+// Demonstrates arrow functions in JavaScript.
+
+const square = (number) => {
+
+    return number * number;
+};
+
+console.log("Square :", square(5));
+
+/*
+==========================================
+Explanation
+
+Arrow functions provide a
+shorter syntax for writing
+functions.
+
+They were introduced in ES6.
+
+When to Use
+
+Use arrow functions for
+short and simple functions.
+
+Expected Output
+
+Square : 25
+
+==========================================
+*/
