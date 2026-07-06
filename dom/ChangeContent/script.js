6724e67bd306325d55b7637f3a6a8af1dac59778
Simranroshangupta <guptasimransushma@gmail.com>
2026-07-06 16:04:56 +0530

    Add DOM content manipulation example

---
+++ dom/ChangeContent/script.js
@@ -0,0 +1,25 @@
+// script.js
+// Demonstrates changing HTML content.
+
+function changeText() {
+
+    const title = document.getElementById("title");
+
+    title.textContent = "Welcome to JavaScript!";
+}
+
+/*
+==========================================
+Explanation
+
+textContent changes
+the text of an HTML element.
+
+When to Use
+
+Use textContent whenever
+you need to update
+text dynamically.
+
+==========================================
+*/
