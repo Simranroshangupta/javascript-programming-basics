6a28b53b97b013063a9608155fde2844156d57a9
Simranroshangupta <guptasimransushma@gmail.com>
2026-07-06 16:05:41 +0530

    Add DOM style manipulation example

---
+++ dom/ChangeStyles/script.js
@@ -0,0 +1,26 @@
+// script.js
+// Demonstrates changing CSS styles.
+
+function changeStyle() {
+
+    const heading = document.getElementById("heading");
+
+    heading.style.color = "blue";
+    heading.style.fontSize = "40px";
+}
+
+/*
+==========================================
+Explanation
+
+The style property is used
+to modify CSS using JavaScript.
+
+When to Use
+
+Use it to change the
+appearance of elements
+dynamically.
+
+==========================================
+*/
