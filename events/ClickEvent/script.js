840dddad5828fb729c4e578f2265bbe9fef0418e
Simranroshangupta <guptasimransushma@gmail.com>
2026-07-06 16:12:57 +0530

    Add click event example

---
+++ events/ClickEvent/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("button");
+
+button.addEventListener("click", function () {
+
+    document.getElementById("message").textContent =
+        "Button Clicked!";
+});
