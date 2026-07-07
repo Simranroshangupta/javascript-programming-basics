66d5f497e435fe3284b58f62adec72983941d5c4
Simranroshangupta <guptasimransushma@gmail.com>
2026-07-07 22:04:42 +0530

    Add destructuring example

---
+++ es6_features/Destructuring.js
@@ -0,0 +1,18 @@
+// Destructuring.js
+
+const student = {
+
+    name: "Simran",
+
+    age: 20,
+
+    course: "AIML"
+};
+
+const { name, age, course } = student;
+
+console.log(name);
+
+console.log(age);
+
+console.log(course);
